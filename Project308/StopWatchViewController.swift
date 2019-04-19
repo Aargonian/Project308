83033f4f969024476beb7706c1835b4aadf10700
Timothy Davis <dtimmy690@gmail.com>
2019-04-19 15:22:37 -0400

    Connected empty swift files for each view.
    Except for WorldClock, because it's in the UTC branch already.

---
+++ Project308/StopWatchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StopwatchViewController.swift
+//  Project308
+//
+//  Created by Davis, Timothy A. on 4/19/19.
+//  Copyright © 2019 Helton, Aaron S. All rights reserved.
+//
+
+import UIKit
+
+class StopWatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

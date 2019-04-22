7928e2d70db149ec9d1496bec43c70cafb75fe54
Andrew Case <andrew_case11@mymail.eku.edu>
2019-04-22 13:46:12 -0400

    added outlets and actions

--- Project308/TimerViewController.swift
+++ Project308/TimerViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class TimerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    @IBAction func clearPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func startStopPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var timePicker: UIPickerView!
+    
     var times = [
-        [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60],[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
+        [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
     ]
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -29,9 +43,7 @@ class TimerViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         // Do any additional setup after loading the view.
     }
     
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        
-    }
+    
 
     
     /*

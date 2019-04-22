187348ec3ff5ea4965dca65e5eddd5fda1f62eaa
Timothy Davis <dtimmy690@gmail.com>
2019-04-22 15:36:55 -0400

    Stopwatch is mostly functional -- more below
    The stopwatch still counts time after the user has pressed stop, and will snap to the total elapsed time after the user presses start.
    The intended effect would be for elapsed time to pause after the user presses stop

--- Project308/StopWatchViewController.swift
+++ Project308/StopWatchViewController.swift
@@ -18,29 +18,84 @@ class StopWatchViewController: UIViewController {
     @IBOutlet weak var clearAllLapsButton: UIButton!
     @IBOutlet weak var clearTimesButton: UIButton!
     
+    var timeStarted : Date? = nil
+    var isRunning : Bool = false
+    
+    
+    func timeLoop()
+    {
+        DispatchQueue.global(qos: .background).async {
+            while(self.isRunning) {
+                let now = Date()
+                let elapsedTime = now.timeIntervalSince(self.timeStarted!)
+                let hours = floor(elapsedTime/3600)
+                let minutes = Int((elapsedTime/60)) % 60
+                let seconds = Int(floor(elapsedTime))%60
+                let millis = Int(floor((elapsedTime * 1000))) % 1000
+                //let timeString = String.init(format: "%02d:%02d:%02d.%03d",
+                                             //hours, minutes, seconds, millis)
+                
+                let hourStr = String.init(format: "%d", hours)
+                let minuteStr = String.init(format: "%02d", minutes)
+                let secondsStr = String.init(format: "%02d", seconds)
+                let millisStr = String.init(format: "%03d", millis)
+                let timeString = "\(hourStr):\(minuteStr):\(secondsStr).\(millisStr)"
+                print("TimeSTR: " + timeString)
+                DispatchQueue.main.async {
+                    self.timeLabel.text! = timeString
+                }
+                usleep(1000)
+            }
+        }
+    }
     
     @IBAction func startButtonPressed(_ sender: UIButton) {
+        startButton.isEnabled = false
+        lapButton.isEnabled = true
+        stopButton.isEnabled = true
         
+        if timeStarted == nil {
+            timeStarted = Date()
+        }
+        isRunning = true
+        timeLoop()
     }
     
     @IBAction func labButtonPressed(_ sender: UIButton) {
+        clearLastLapButton.isEnabled = true
+        clearAllLapsButton.isEnabled = true
         
     }
-        
+    
     @IBAction func stopButtonPressed(_ sender: UIButton) {
+        startButton.isEnabled = true
+        stopButton.isEnabled = false
+        clearTimesButton.isEnabled = true
+        
+        isRunning = false
         
     }
     
     @IBAction func clearLastLapButtonPressed(_ sender: UIButton) {
-        
+        if lapTimeTextView.text! == "" {
+            clearLastLapButton.isEnabled = false
+            clearAllLapsButton.isEnabled = false
+        } else {
+            
+        }
     }
     
     @IBAction func clearAllLapsButtonPressed(_ sender: UIButton) {
+        clearLastLapButton.isEnabled = false
+        clearAllLapsButton.isEnabled = false
         
+        lapTimeTextView.text! = ""
     }
     
     @IBAction func clearTimeButtonPressed(_ sender: UIButton) {
+        startButton.isEnabled = true
         
+        timeLabel.text! = "00:00:00.000"
     }
     
     

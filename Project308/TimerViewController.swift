3387d557c568931c362e726bbeab5986f5172512
Andrew Case <andrew_case11@mymail.eku.edu>
2019-04-22 13:38:39 -0400

    started work on timer, mostly ui bs

--- Project308/TimerViewController.swift
+++ Project308/TimerViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class TimerViewController: UIViewController {
+class TimerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    var times = [
+        [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60],[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
+    ]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return times[component].count
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +29,11 @@ class TimerViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+    }
 
+    
     /*
     // MARK: - Navigation
 

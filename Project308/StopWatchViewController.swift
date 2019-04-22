4ba4394d539e2abbdc72344f9d0e9037b456e352
Timothy Davis <dtimmy690@gmail.com>
2019-04-22 13:46:08 -0400

    Created appropriate outlets and actions for Stopwatch.

--- Project308/StopWatchViewController.swift
+++ Project308/StopWatchViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class StopWatchViewController: UIViewController {
-
+    @IBOutlet weak var lapTimeTextView: UITextView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var lapButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var clearLastLapButton: UIButton!
+    @IBOutlet weak var clearAllLapsButton: UIButton!
+    @IBOutlet weak var clearTimesButton: UIButton!
+    
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func labButtonPressed(_ sender: UIButton) {
+        
+    }
+        
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clearLastLapButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clearAllLapsButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clearTimeButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
